af48e379164f0b91398f973130414aba805406a0
Junho Yeo <i@junho.io>
2022-02-09 01:23:01 +0900

    PoC for rendering images

--- src/api/InviteResolver.ts
+++ src/api/InviteResolver.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+import { createCanvas, Image } from '@napi-rs/canvas';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { allowCORS } from './_lib/allowCORS';
@@ -9,8 +12,30 @@ type APIRequest = VercelRequest & {
   };
 };
 
+type url = string;
+const getBufferFromURL = async (url: url): Promise<Buffer> => {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  return Buffer.from(response.data, 'utf-8');
+};
+
+type ImageInput = {
+  url: url;
+  width: number;
+  height: number;
+};
+const getImageFromURL = async (params: ImageInput) => {
+  const img = new Image();
+  img.src = await getBufferFromURL(params.url);
+  img.width = params.width;
+  img.height = params.height;
+  return { image: img, ...params };
+};
+const loadImages = (images: ImageInput[]) =>
+  Promise.all(images.map(getImageFromURL));
+
 const InviteResolver = async (req: APIRequest, res: VercelResponse) => {
   const inviteId = req.query.inviteId;
+  console.log({ inviteId });
   if (!inviteId) {
     res.status(400).json({ status: 'error', message: 'InviteId not found' });
     return;
@@ -33,7 +58,7 @@ const InviteResolver = async (req: APIRequest, res: VercelResponse) => {
     ? `https://cdn.discordapp.com/splashes/${inviteDetail.guild.id}/${inviteDetail.guild.splash}.jpg?size=4096`
     : null;
 
-  res.json({
+  const details = {
     name,
     description,
     channel,
@@ -42,8 +67,66 @@ const InviteResolver = async (req: APIRequest, res: VercelResponse) => {
     inviteURL,
     iconURL,
     splashURL,
-  });
-  return;
+  };
+  const [iconImage, splashImage] = await loadImages([
+    { url: details.iconURL, width: 200, height: 200 },
+    { url: details.splashURL, width: 1280, height: 640 },
+  ]);
+  const canvas = createCanvas(1280, 640);
+
+  const context = canvas.getContext('2d');
+  context.drawImage(
+    splashImage.image,
+    0,
+    0,
+    splashImage.width,
+    splashImage.height,
+  );
+
+  // prepare clip
+  const x = 100;
+  const y = 100;
+  const radius = iconImage.width / 2;
+  const width = iconImage.width;
+  const height = iconImage.height;
+  context.beginPath();
+  context.moveTo(x + radius, y);
+  context.lineTo(x + width - radius, y);
+  context.quadraticCurveTo(x + width, y, x + width, y + radius);
+  context.lineTo(x + width, y + height - radius);
+  context.quadraticCurveTo(
+    x + width,
+    y + height,
+    x + width - radius,
+    y + height,
+  );
+  context.lineTo(x + radius, y + height);
+  context.quadraticCurveTo(x, y + height, x, y + height - radius);
+  context.lineTo(x, y + radius);
+  context.quadraticCurveTo(x, y, x + radius, y);
+  context.closePath();
+  context.clip();
+
+  // draw and restore context
+  context.drawImage(
+    iconImage.image,
+    x + 5,
+    y + 5,
+    iconImage.width - 10,
+    iconImage.height - 10,
+  );
+  context.closePath();
+  context.restore();
+
+  const buffer = await canvas.encode('png');
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', `image/png`);
+  res.setHeader(
+    'Cache-Control',
+    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`,
+  );
+  res.end(buffer);
 };
 
 export default allowCORS(InviteResolver);
