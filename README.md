cd035706b5d174f61f1852011d466689b7304532
Junho Yeo <i@junho.io>
2022-02-08 21:58:48 +0900

    Initialize project
    From template https://github.com/junhoyeo/feature-oriented-next

---
+++ README.md
@@ -0,0 +1,1 @@
+# discord.alphaworks.team
---
+++ src/Landing/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LandingPage = () => {
+  return <Container />;
+};
+
+export default LandingPage;
+
+const Container = styled.div``;
