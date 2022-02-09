9cb4f3e1c2da68cd3940cc23155abc278506efcb
Junho Yeo <i@junho.io>
2022-02-09 15:44:03 +0900

    Wrap `link` tags and `GlobalStyle` with `next/head`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 
 import { GlobalStyle } from '@/components/GlobalStyle';
@@ -5,25 +6,27 @@ import { GlobalStyle } from '@/components/GlobalStyle';
 function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <GlobalStyle />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <GlobalStyle />
+      </Head>
       <Component {...pageProps} />
     </React.Fragment>
   );
