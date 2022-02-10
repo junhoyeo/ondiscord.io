a1cd3404a85c3475c7c7f317241280d2e32e7081
Junho Yeo <i@junho.io>
2022-02-10 14:08:29 +0900

    Redirect root to GitHub

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const withSvgr = require('next-plugin-svgr');
 
 module.exports = withSvgr({
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://github.com/junhoyeo/ondiscord.io',
+        permanent: false,
+      },
+    ];
+  },
 });
